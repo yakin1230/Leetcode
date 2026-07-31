92d622b4bf31a96e9932dbef6d35ce7bc9cce829
yakin1230 <ahmadyakin302512@gmail.com>
2026-07-31 23:54:15 +0530

    Time: 9 ms (37.57%), Space: 63.9 MB (54.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,14 @@
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+
+    let original = x;
+    let reversed = 0;
+
+    while (x > 0) {
+        let digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x = Math.floor(x / 10);
+    }
+
+    return original === reversed;
+};
